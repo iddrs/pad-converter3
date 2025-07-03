a0df53f511acd4d9f55985b5fdeab11697737535
Everton da Rosa <everton3x@gmail.com>
2025-07-03 10:39:37 -0300

    fix: Cálculo dos saldos finais de restos a pagar
    O cálculo do saldo final de restos a pagar foi alterado pra que os restos não processados liquidados constem como processados.

--- main.php
+++ main.php
@@ -8,13 +8,13 @@ require 'vendor/autoload.php';
 
 echo "Conversão dos TXT do PAD.", PHP_EOL;
 echo "Digite os dados solicitados.", PHP_EOL;
-/*
+
 echo "Ano [AAAA]: ";
 fscanf(STDIN, "%d\n", $ano);
 echo "Mês [MM] (1 ~ 12): ";
 fscanf(STDIN, "%d\n", $mes);
 $mes = str_pad($mes, 2, '0', STR_PAD_LEFT);
-*/
+
 /*
  * Configurações
  */
@@ -30,10 +30,10 @@ $source_dirs = [
     ".\\source\\fpsm\\",
     ".\\source\\cm\\",
 ];
-$remessa = (int) $ano.$mes;
+$remessa = (int) $ano . $mes;
 
 
-$layoutrepo= new App\Layout\LayoutRepository($layout_dir);
+$layoutrepo = new App\Layout\LayoutRepository($layout_dir);
 $sourcerepo = new App\Source\SourceRepository(...$source_dirs);
 $outputrepo = new \App\Output\OutputRepository('host=localhost port=5432 dbname=pmidd user=postgres password=lise890', $remessa);
 $converter = new App\Convert\Converter($layoutrepo, $sourcerepo, $outputrepo, $remessa);
